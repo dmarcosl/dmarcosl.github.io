e45495136d900c70c69592a56ed5600f6f8ca826
dmarcosl <danielmarcoslorrio93@gmail.com>
2025-02-15 15:15:41 +0000

    Deploying to gh-pages from @ dmarcosl/dmarcosl.github.io@e72a40ed8a89d44f14c710aa7b2b658978c8e1f8 🚀

---
+++ _next/static/3VpXKU6t49A8L0AqpmL5Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/cb355538-458574df043fc6eb.js","static/chunks/562-351138d63e6151e6.js","static/chunks/pages/index-feca7dee78861cc0.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
